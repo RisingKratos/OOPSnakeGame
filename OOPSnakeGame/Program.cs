365b956649de54811dd4773741bf9a1311ae8fa8
RisingKratos <risingkratos@gmail.com>
2016-01-25 23:08:57 +0600

    SnakeDemo

---
+++ OOPSnakeGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSnakeGame
+{
+    interface IDrawable
+    {
+        void Draw();
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.SetWindowSize(50, 50);
+
+            Snake snake = new Snake(25, 25);
+
+        }
+    }
+}
---
+++ OOPSnakeGame/Snake.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSnakeGame
+{
+    public class Snake : IDrawable
+    {
+        char mark = 's';
+        List<Point> corpus = null;
+        public Snake(int X, int Y)
+        {
+            corpus = new List<Point>();
+            corpus.Add(new Point { x = X, y = Y }); //узнать, как это использовать с this.x = x, this.y = y
+            Draw();
+        }
+
+        public void Draw()
+        {
+            foreach (Point point in corpus)
+            {
+                Console.SetCursorPosition(point.x, point.y);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(mark);
+            }
+        }
+
+        public void Clear()
+        {
+            foreach (Point point in corpus)
+            {
+                Console.SetCursorPosition(point.x, point.y);
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write(' ');
+            }
+        }
+
+    }
+}
